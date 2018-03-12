a4c36d25a1b77510125c2d69d045cda5a0439e70
zxbit2011 <zxbit2011@163.com>
2018-03-12 18:15:46 +0800

    --

--- start.go
+++ start.go
@@ -4,7 +4,11 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	//"github.com/beewit/beekit/utils"
+	"fmt"
+	"net/http"
+
 	"github.com/beewit/beekit/utils"
+	"github.com/beewit/beekit/utils/uhttp"
 )
 
 func main() {
@@ -25,6 +29,32 @@ func main() {
 	//微信域名验证码
 	e.File("MP_verify_3Z6AKFClzM8nQt3q.txt", "page/MP_verify_3Z6AKFClzM8nQt3q.txt")
 	e.File("/.well-known/pki-validation/fileauth.txt", "fileauth.txt")
+
+	var hiveHost = "http://hive.9ee3.com"
+
+	e.GET("/api/help/list", func(c echo.Context) error {
+		t := c.FormValue("t")
+		url := fmt.Sprintf("%s/api/help/list?t=%s", hiveHost, t)
+		b, err := uhttp.PostForm(url, nil)
+		if err != nil {
+			return utils.ErrorNull(c, "获取失败")
+		}
+		rp := utils.ToResultParam(b)
+		return c.JSON(http.StatusOK, rp)
+	})
+	e.GET("/api/help/get", func(c echo.Context) error {
+		id := c.FormValue("id")
+		if id == "" || !utils.IsValidNumber(id) {
+			return utils.ErrorNull(c, "数据参数错误")
+		}
+		url := fmt.Sprintf("%s/api/help/get?id="+id, hiveHost)
+		b, err := uhttp.PostForm(url, nil)
+		if err != nil {
+			return utils.ErrorNull(c, "获取失败")
+		}
+		rp := utils.ToResultParam(b)
+		return c.JSON(http.StatusOK, rp)
+	})
 
 	utils.Open("http://127.0.0.1:8080")
 
